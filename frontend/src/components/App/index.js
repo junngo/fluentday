780bb327804cdaf6e7cbf013f65cd669348b067d
junngo <myeongjun.ko@gmail.com>
2020-03-18 23:54:15 +0900

    creating record component

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import 'components/App/styles.scss';
+import Record from 'components/Record'
 
 function App() {
   return (
     <div className="App">
       Hello React
+      <Record />
     </div>
   );
 }
