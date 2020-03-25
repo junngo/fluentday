b906b109a406ad1e03da3158ebbf58b45d9e88f4
junngo <myeongjun.ko@gmail.com>
2020-03-25 23:54:17 +0900

    created blink component

---
+++ frontend/src/components/Blink/container.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Blink from "./presenter";
+
+const Container = props => <Blink {...props} />;
+
+export default Container;
---
+++ frontend/src/components/Blink/presenter.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+
+const Blink = (props, context) => <h1>Blink</h1>;
+
+export default Blink;
